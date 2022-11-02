8972ce680450248878aad0cc9e3785b239183b5d
Antoine BLANGY <os.ablangy@laposte.net>
2022-11-02 13:19:07 +0100

    main will wait for SIGTERM or SIGQUIT
    The defaut behavior of this app is to wait for SIGTERM or SIGQUIT.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: Unlicense
 
+#include <cstdlib>
 #include <iostream>
+
+#include <signal.h>
+#include <string.h>
 
 #include "g3log/loglevels.hpp"
 #include "g3log/logmessage.hpp"
@@ -11,21 +15,58 @@
 
 #include "Arguments.h"
 
-int main(int argc, char ** argv)
+std::map<int, std::string_view> g_termSigs = {
+	{ SIGTERM, "SIGTERM" },
+	{ SIGQUIT, "SIGQUIT" },
+};
+sigset_t g_termSigSet;
+
+static bool prepareTerminationSignal()
+{
+	struct sigaction termAction;
+
+	memset(&termAction, 0, sizeof(termAction));
+	termAction.sa_handler = SIG_IGN;
+	sigemptyset(&g_termSigSet);
+
+	for (auto sig : g_termSigs) {
+		if (sigaction(sig.first, &termAction, nullptr) < 0) {
+			std::cerr << "sigaction(" << sig.second << ") has failed: " << errno << std::endl;
+			return false;
+		}
+
+		if (sigaddset(&g_termSigSet, sig.first) < 0) {
+			std::cerr << "sigaddset(" << sig.second << ") has failed: " << errno << std::endl;
+			return false;
+		}
+	}
+
+	if (pthread_sigmask(SIG_BLOCK, &g_termSigSet, nullptr) < 0) {
+		std::cerr << "pthread_sigmask() has failed: " << errno << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+int main(int argc, char** argv)
 {
 	auto& args = tapp::Arguments::instance();
 	if (args.set(argc, argv) != tapp::Arguments::eSetReturnCode::CONTINUE) {
 		args.usage();
 		std::exit(EXIT_FAILURE);
 	}
-	
+
+	// We have to configure termination signals handling before creating the threads in order for them to inherit our signal mask
+	prepareTerminationSignal();
+
 	auto logWorker = g3::LogWorker::createLogWorker();
 	if (not logWorker) {
 		std::cerr << "Échec d'initialisation du module de trace (pas de worker)\n";
 		std::exit(EXIT_FAILURE);
 	}
 
-	auto logRotateSink = std::make_unique<LogRotate> (args.getLogFilenamePrefix(), args.getLogDirectory());
+	auto logRotateSink = std::make_unique<LogRotate>(args.getLogFilenamePrefix(), args.getLogDirectory());
 	logRotateSink->setMaxArchiveLogCount(args.getMaxArchiveLogCount());
 	logRotateSink->setMaxLogSize(args.getMaxLogSize());
 	logRotateSink->setFlushPolicy(args.getFlushPolicy());
@@ -38,8 +79,6 @@ int main(int argc, char ** argv)
 
 	g3::initializeLogging(logWorker.get());
 
-	LOG(INFO) << "Hello from test app !";
-
 	// SIGTERM default behavior restored
 	g3::overrideSetupSignals({
 	    { SIGABRT, "SIGABRT" },
@@ -48,7 +87,22 @@ int main(int argc, char ** argv)
 	    { SIGSEGV, "SIGSEGV" },
 	});
 
+	LOG(INFO) << "Hello from test app !";
+
+	auto signum = sigwaitinfo(&g_termSigSet, nullptr);
+	if (signum < 0) {
+		std::cerr << "Failed to wait for termination signal: " << errno;
+		return EXIT_FAILURE;
+	}
+
+	auto found = g_termSigs.find(signum);
+	if (found != g_termSigs.end()) {
+		std::cout << found->second << " received, stop test app";
+	} else {
+		std::cout << "Signal: " << signum << " received, stop test app";
+	}
+
 	g3::internal::shutDownLogging();
 
-	return 0;
+	return EXIT_SUCCESS;
 }
